5dffa4509c78c366fdeed470638e9dd229cfc0c3
markov98 <74935630+markov98@users.noreply.github.com>
2023-05-04 22:27:21 +0300

    modified phonbook.js

--- phonebook.js
+++ phonebook.js
@@ -1,5 +1,14 @@
-/* TODO: 
-	create phonebook array
-	add methods for adding in the phonebook and getting it
-	export the methods
-*/
+let phonebook = [];
+
+function getPhonebook() {
+	return phonebook;
+}
+
+function addContact(contact) {
+	phonebook.push(contact);
+}
+
+module.exports = {
+	getPhonebook,
+	addContact
+}
