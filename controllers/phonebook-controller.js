4cd88808afdd40a5adee10bf785c8fcb7442c38c
markov98 <74935630+markov98@users.noreply.github.com>
2023-05-05 18:01:15 +0300

    adding code to the index method inside phonbook-controller.js

--- controllers/phonebook-controller.js
+++ controllers/phonebook-controller.js
@@ -2,8 +2,9 @@ const phonebook = require('../phonebook');
 
 module.exports = {
   index: (req, res) => {
-    res.render('index');
-    // TODO: load index page
+    res.render('index', {
+      contacts: phonebook.getPhonebook()
+    });
   },
   addPhonebookPost:(req, res) => {
     // TODO: add a phonebook object to the array
